dca13a9fa2427331f2f0a0ef6573feb519cb0723
Igor Theodoro <igortheodoro12@gmail.com>
2020-09-04 16:09:48 -0300

    Updates

---
+++ precache-manifest.52ac5acc71b09bd068604397eb9933d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99697733c106c9691271c16be6b7af1",
+    "url": "/front-end-url-shortener/index.html"
+  },
+  {
+    "revision": "ad6c6e6b6ee39699009d",
+    "url": "/front-end-url-shortener/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2cac13e68fd74627fbd1",
+    "url": "/front-end-url-shortener/static/js/2.3e48db81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/front-end-url-shortener/static/js/2.3e48db81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6c6e6b6ee39699009d",
+    "url": "/front-end-url-shortener/static/js/main.d87d1cff.chunk.js"
+  },
+  {
+    "revision": "b97424d90d67eaf0c609",
+    "url": "/front-end-url-shortener/static/js/runtime-main.bfb26384.js"
+  }
+]);
